92a9d3083f0c381597587a2ceb555cd1ca86cda5
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-01 13:13:55 +0800

    add new created files info

---
+++ src/main/kotlin/com/laibao/kotlin/springbootkotlin/blog/SpringBootKotlinBlogApplication.kt
@@ -0,0 +1,21 @@
+package com.laibao.kotlin.springbootkotlin.blog
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinBlogApplication {
+
+    private val logger: Logger = LoggerFactory.getLogger(SpringBootKotlinBlogApplication::class.java)
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<SpringBootKotlinBlogApplication>(*args)
+        }
+    }
+}
+
+
