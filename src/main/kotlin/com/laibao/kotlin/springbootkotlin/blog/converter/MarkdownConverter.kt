0d74ff36540b4beb54bcb96025bdde187612c3f9
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-01 15:56:41 +0800

    add new application.properties file info

---
+++ src/main/kotlin/com/laibao/kotlin/springbootkotlin/blog/converter/MarkdownConverter.kt
@@ -0,0 +1,23 @@
+package com.laibao.kotlin.springbootkotlin.blog.converter
+
+import org.commonmark.ext.autolink.AutolinkExtension
+import org.commonmark.parser.Parser
+import org.commonmark.renderer.html.HtmlRenderer
+import org.springframework.stereotype.Service
+
+/**
+ * @author laibao wang
+ */
+@Service
+class MarkdownConverter : (String?) -> String{
+    private val parser = Parser.builder().extensions(listOf(AutolinkExtension.create())).build()
+
+    private val renderer = HtmlRenderer.builder().build()
+
+    override fun invoke(input: String?): String {
+        if (input == null || input == "") {
+            return ""
+        }
+        return renderer.render(parser.parse(input))
+    }
+}
